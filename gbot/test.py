870da9d3857fe036e0daa4425dabf3da67d1073e
Gbenga Adeyi <adeyigbenga005@gmail.com>
2021-12-16 19:34:12 +0100

    Add boilerplate for tests

---
+++ gbot/test.py
@@ -0,0 +1,17 @@
+import unittest
+
+class GBotTest(unittest.TestCase):
+    def test_bot_can_greet(self):
+        pass
+
+    def test_bot_can_send_message(self):
+        pass
+
+    def test_bot_can_accept_command(self):
+        pass
+
+    def test_bot_can_send_broadcast_message(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
